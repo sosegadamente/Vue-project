a4b689e41ed7e646bdc61e8e78f12a1f16494631
sosegadamente <149854564+sosegadamente@users.noreply.github.com>
2024-12-05 17:09:23 +0900

    update
    style scope

--- src/views/User.vue
+++ src/views/User.vue
@@ -2,3 +2,13 @@
 <template>
   <p>This page is {{ $route.params.id }}'s page</p>
 </template>
+<style>
+p {
+  text-transform: initial;
+}
+</style>
+<style scoped>
+p {
+  text-transform: capitalize;
+}
+</style>
